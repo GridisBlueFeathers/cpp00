d40615afe5fad8f847ce691a48e33c7ab433785c
Serhii Veretennikov <gridis72@gmail.com>
2025-04-02 14:00:25 +0200

    feat: implemented ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 13:20:08 by svereten          #+#    #+#             */
+/*   Updated: 2025/04/02 13:49:52 by svereten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (unsigned char)std::toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
